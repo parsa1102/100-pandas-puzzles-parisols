8b1b7adb95093f9343299eb92794f2a85990e0b7
Parsa <parsa.gholipour.sh@gmail.com>
2024-05-15 14:53:45 +0330

    1-13 solved and files are now filled with their respective solution

---
+++ solutions/q008.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+
+data = {'animal': ['cat', 'cat', 'snake', 'dog', 'dog', 'cat', 'snake', 'cat', 'dog', 'dog'],
+        'age': [2.5, 3, 0.5, np.nan, 5, 2, 4.5, np.nan, 7, 3],
+        'visits': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+        'priority': ['yes', 'yes', 'no', 'yes', 'no', 'no', 'no', 'yes', 'no', 'no']}
+
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+
+df = pd.DataFrame(data=data, index=labels)
+
+newDf= df.iloc[[3, 4, 8]][['animal', 'age']]
+print(newDf)
